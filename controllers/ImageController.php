6d0bb00d83a2d736b5cd1500e3370893f4098919
Duc Hoang <duc.hoang@globee.hk>
2021-06-05 10:26:44 +0700

    adsad

--- controllers/ImageController.php
+++ controllers/ImageController.php
@@ -1,7 +1,11 @@
 <?php
 
 class ImageController {
+    // pmfbdjvhoa.jpg
     public function get($argv) {
-        
+        $imageType = strtolower(pathinfo($argv['params']['hinh'], PATHINFO_EXTENSION));
+        $image = file_get_contents(realpath(dirname(getcwd()))."/storage/".$argv['params']['hinh']);
+        header("Content-type: image/".$imageType);
+        echo $image;
     }
 }
