57894200c243e8e057284de9dcd09485f905681a
Duc Hoang <duc.hoang@globee.hk>
2021-06-12 20:48:48 +0700

    ok

--- controllers/CapPhatController.php
+++ controllers/CapPhatController.php
@@ -14,7 +14,7 @@ class CapPhatController {
         $capphatDb = new CapPhatDatabase();
         Response::json(array(
             'request' => $argv,
-            'viewData' => $capphatDb->thongKeCau2a($argv),
+            'viewData' => $capphatDb->thongKeCau2a(),
             'success' => true
         ));
     }
@@ -23,7 +23,7 @@ class CapPhatController {
         $capphatDb = new CapPhatDatabase();
         Response::json(array(
             'request' => $argv,
-            'viewData' => $capphatDb->thongKeCau2b($argv),
+            'viewData' => $capphatDb->thongKeCau2b(),
             'success' => true
         ));
     }
@@ -32,7 +32,7 @@ class CapPhatController {
         $capphatDb = new CapPhatDatabase();
         Response::json(array(
             'request' => $argv,
-            'viewData' => $capphatDb->thongKeCau2c($argv),
+            'viewData' => $capphatDb->thongKeCau2c(),
             'success' => true
         ));
     }
@@ -41,7 +41,68 @@ class CapPhatController {
         $capphatDb = new CapPhatDatabase();
         Response::json(array(
             'request' => $argv,
-            'viewData' => $capphatDb->thongKeCau2d($argv),
+            'viewData' => $capphatDb->thongKeCau2d(),
+            'success' => true
+        ));
+    }
+
+    public function thongKeDefault($argv) {
+        $capphatDb = new CapPhatDatabase();
+        Response::json(array(
+            'request' => $argv,
+            'viewData' => $capphatDb->thongKeDefault(),
+            'success' => true
+        ));
+    }
+
+    public function selectWithVPPandNVwherePB($argv) {
+        // http://localhost/CapPhatController-selectWithVPPandNVwherePB?mapb=PB01
+        $capphatDb = new CapPhatDatabase();
+        Response::json(array(
+            'request' => $argv,
+            'viewData' => $capphatDb->selectWithVPPandNVwherePB($argv['params']['mapb']),
+            'success' => true
+        ));
+    }
+
+    public function select_listVPP_withPB($argv) {
+        // http://localhost/CapPhatController-select_listVPP_withPB?mapb=PB01
+        $capphatDb = new CapPhatDatabase();
+        Response::json(array(
+            'request' => $argv,
+            'viewData' => $capphatDb->select_listVPP_withPB($argv['params']['mapb']),
+            'success' => true
+        ));
+    }
+
+    public function select_listNV_withVPP_andPB($argv) {
+        // http://localhost/CapPhatController-select_listNV_withVPP_andPB?mapb=PB01&mavpp=VPP01
+        $capphatDb = new CapPhatDatabase();
+        Response::json(array(
+            'request' => $argv,
+            'viewData' => $capphatDb->select_listNV_withVPP_andPB($argv['params']['mapb'], $argv['params']['mavpp']),
+            'success' => true
+        ));
+    }
+
+    public function countVPPfromPB($argv) {
+        // http://localhost/CapPhatController-countVPPfromPB?mapb=PB01
+        // http://localhost/CapPhatController-countVPPfromPB
+        $capphatDb = new CapPhatDatabase();
+        Response::json(array(
+            'request' => $argv,
+            'viewData' => $capphatDb->countVPPfromPB($argv['params']),
+            'success' => true
+        ));
+    }
+
+    public function baocaoQuery($argv) {
+        // http://localhost/CapPhatController-baocaoQuery?mapb=PB01
+        // http://localhost/CapPhatController-baocaoQuery?manv=NV01
+        $capphatDb = new CapPhatDatabase();
+        Response::json(array(
+            'request' => $argv,
+            'viewData' => $capphatDb->baocaoQuery($argv['params']),
             'success' => true
         ));
     }
--- databases/CapPhatDatabase.php
+++ databases/CapPhatDatabase.php
@@ -124,22 +124,113 @@ class CapPhatDatabase extends Database implements DatabaseInterface {
     }
 
     public function selectWithVPPandNVwherePB(string $mapb) {
-
+        $result = mysqli_query($this->conn,"
+        SELECT B.TENVPP, A.MAPB , A.TENPB, A.TONGSL FROM
+        (SELECT * FROM vanphongpham) as B LEFT JOIN
+        (SELECT CP.SOPHIEU, CP.NGAYCAP, NV.MANV, NV.HOTEN, PB.MAPB ,PB.TENPB, VPP.MAVPP, VPP.TENVPP, 
+        SUM(CP.SOLUONG) AS TONGSL FROM
+        capphat CP, nhanvien NV, phongban PB, vanphongpham VPP
+        WHERE CP.MAVPP = VPP.MAVPP
+        AND CP.MANV = NV.MANV
+        AND NV.MAPB = PB.MAPB
+        AND PB.MAPB = '".$mapb."'
+        GROUP BY CP.MAVPP) as A
+        ON A.MAVPP = B.MAVPP
+        ");
+        $data = array();
+        for ($i = 0; $i < mysqli_num_rows($result); $i++){
+            array_push($data, mysqli_fetch_assoc($result));
+        }
+        return $data;
     }
 
     public function select_listVPP_withPB(string $mapb) {
-
+        $result = mysqli_query($this->conn,"
+        SELECT DISTINCT L.MAVPP, L.TENVPP, L.DVT, L.GIANHAP*SUM(R.SOLUONG) AS TRIGIA FROM
+        ( SELECT * FROM VANPHONGPHAM ) AS L
+        JOIN
+        (SELECT CP.MAVPP, CP.SOLUONG, CP.MANV ,NV.HOTEN,NV.MAPB FROM CAPPHAT AS CP JOIN 
+        NHANVIEN AS NV ON CP.MANV = NV.MANV ) AS R
+        ON L.MAVPP = R.MAVPP WHERE R.MAPB = '".$mapb."'
+        GROUP BY R.MAVPP
+        ");
+        $data = array();
+        for ($i = 0; $i < mysqli_num_rows($result); $i++){
+            array_push($data, mysqli_fetch_assoc($result));
+        }
+        return $data;
     }
 
     public function select_listNV_withVPP_andPB(string $mapb, string $mavpp) {
-
+        $result = mysqli_query($this->conn,"
+        SELECT DISTINCT  R.MANV , R.HOTEN, SUM(L.SOLUONG) AS SOLUONGMUON FROM
+        ( SELECT * FROM VANPHONGPHAM ) AS L
+        JOIN
+        (SELECT CP.MAVPP, CP.SOLUONG, CP.MANV ,NV.HOTEN,NV.MAPB FROM CAPPHAT 
+        AS CP JOIN NHANVIEN AS NV ON CP.MANV = NV.MANV ) AS R
+        ON L.MAVPP = R.MAVPP WHERE R.MAPB = '".$mapb."' AND R.MAVPP = '".$mavpp."'
+        GROUP BY R.MAVPP, R.MANV
+        ");
+        $data = array();
+        for ($i = 0; $i < mysqli_num_rows($result); $i++){
+            array_push($data, mysqli_fetch_assoc($result));
+        }
+        return $data;
     }
 
     public function countVPPfromPB($params) { // <--- There are 2 functions in this method
-
+        $sql = isset($params['mapb']) ? 
+        "
+        SELECT NV.MAPB, SUM(SOLUONG) AS TONGSL
+        FROM CAPPHAT CP JOIN NHANVIEN NV
+        ON CP.MANV = NV.MANV
+        AND NV.MAPB = 'PB01'
+        GROUP BY MAPB
+        " : 
+        "
+        SELECT L.TENPB, COALESCE(SUM(R.SOLUONG), 0 ) AS SOLUONG FROM
+        PHONGBAN AS L
+        LEFT JOIN
+        (SELECT CP.*,NV.MAPB FROM CAPPHAT CP JOIN NHANVIEN NV ON CP.MANV = NV.MANV) AS R
+        ON L.MAPB = R.MAPB GROUP BY L.MAPB
+        ";
+        $result = mysqli_query($this->conn, $sql);
+        $data = array();
+        for ($i = 0; $i < mysqli_num_rows($result); $i++){
+            array_push($data, mysqli_fetch_assoc($result));
+        }
+        return $data;
     }
 
-    public function BaocaoQuery($params) { // <--- There are 2 functions in this method
+    public function baocaoQuery($params) { // <--- There are 2 functions in this method
+        $sql = "";
+        
+        if (isset($params['mapb'])) {
+            $sql = "
+            SELECT BC.SOPHIEU,BC.MANV,BC.NGAYCAP,BC.TENVPP,BC.TRIGIA FROM (SELECT R.SOPHIEU, R.NGAYCAP, R.TENVPP, R.TRIGIA, L.MANV, L.MAPB FROM
+            (SELECT NV.*, PB.TENPB FROM NHANVIEN NV JOIN PHONGBAN PB ON NV.MAPB = PB.MAPB ) AS L
+            JOIN
+            (SELECT CP.SOPHIEU, CP.NGAYCAP, VPP.TENVPP, CP.SOLUONG*VPP.GIANHAP AS TRIGIA ,CP.MANV
+            FROM CAPPHAT CP JOIN VANPHONGPHAM VPP
+            ON CP.MAVPP = VPP.MAVPP) AS R
+            ON L.MANV = R.MANV) AS BC WHERE BC.MAPB = '".$params['mapb']."'
+            ";
+        }
 
+        if (isset($params['manv'])) {
+            $sql = "
+            SELECT SOPHIEU, NGAYCAP, TENVPP, TRIGIA FROM 
+            (SELECT CP.SOPHIEU, NGAYCAP, TENVPP, CP.SOLUONG * GIANHAP AS TRIGIA, MANV
+            FROM CAPPHAT CP JOIN VANPHONGPHAM VPP ON CP.MAVPP = VPP.MAVPP 
+            WHERE CP.MANV = '".$params['manv']."') AS A
+            ";
+        }
+
+        $result = mysqli_query($this->conn, $sql);
+        $data = array();
+        for ($i = 0; $i < mysqli_num_rows($result); $i++){
+            array_push($data, mysqli_fetch_assoc($result));
+        }
+        return $data;
     }
 }
